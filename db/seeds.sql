966bb4934b62fe4e6996786207775c250ee95945
andrewmiddleton1 <66552488+andrewmiddleton1@users.noreply.github.com>
2020-11-16 12:52:04 +1100

    first version for upload with all functions working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name, devoured) VALUES ('Zinger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Quarter Pounder', true);
+
